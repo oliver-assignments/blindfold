ca114b117abcb3dcc3877d2e5618dd8339a1c373
Oliver <oliverbarnum32@gmail.com>
2016-04-16 14:54:53 -0400

    Create wind

---
+++ Assets/Scripts/Wind.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wind : MonoBehaviour {
+
+    SphereCollider collider;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
