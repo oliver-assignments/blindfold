8d01fa1ba9b80bd6a6a13f8ab58338a1e1468212
Oliver Barnum (RIT Student) <oeb9106@ad.rit.edu>
2016-05-09 15:57:49 -0400

    Add throwy knives

--- Assets/Scripts/KnifeThrow.cs
+++ Assets/Scripts/KnifeThrow.cs
@@ -7,7 +7,6 @@ public class KnifeThrow : MonoBehaviour {
     [SerializeField]
 	private float speed = 3;
     [SerializeField]
-    public CharacterController charControl;
     private Vector2 vel;
 	// Use this for initialization
 	void Start () {}
@@ -16,26 +15,28 @@ public class KnifeThrow : MonoBehaviour {
         transform.forward = shooter.transform.up;
         transform.position = shooter.transform.position + transform.forward;
 		vel = transform.forward * speed;
+        GetComponent<Rigidbody2D>().AddForce(vel*100000);
 	}
 	// Update is called once per frame
 	void Update () 
 	{
 		lifespan -= Time.deltaTime;
-		if(lifespan <= 0)
-		{
-			Destroy(gameObject); 
-		}
-		else
-		{
-			charControl.Move(vel * Time.deltaTime);
-            if(charControl.collisionFlags != CollisionFlags.None)
-            {
-                //Embed self in object, stop moving.
-                transform.position += transform.forward*0.1f;
-                vel = Vector2.zero;
+        if (lifespan <= 0)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
 
-                //TODO: If object is player, delete self immediately (alternatively: "fall to floor" by setting z-pos to something >=1)
-            }
-		}
+            //	charControl.Move(vel * Time.deltaTime);
+            //          if(charControl.collisionFlags != CollisionFlags.None)
+            //          {
+            //              //Embed self in object, stop moving.
+            //              transform.position += transform.forward*0.1f;
+            //              vel = Vector2.zero;
+
+            //              //TODO: If object is player, delete self immediately (alternatively: "fall to floor" by setting z-pos to something >=1)
+            //          }
+        }
 	}
 }
