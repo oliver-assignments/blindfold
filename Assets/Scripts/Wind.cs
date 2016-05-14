0d9489855f884ddfb6eff665fe1681c2768965de
Oliver <oliverbarnum32@gmail.com>
2016-05-14 19:01:44 -0400

    Add varying opacity to wind

---
+++ Assets/Scripts/Wind.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wind : MonoBehaviour {
+
+
+	private MeshRenderer renderer;
+	private Rigidbody2D rigidBody;
+	public float magnitudeForOpaque =10;
+	public bool varyingOpacity = true;
+	// Use this for initialization
+	void Start () {
+		renderer = GetComponent<MeshRenderer> ();
+		rigidBody = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (varyingOpacity) {
+			if (rigidBody.velocity.magnitude > magnitudeForOpaque) {
+				renderer.material.color = new Color (
+				renderer.material.color.r,
+				renderer.material.color.g,
+				renderer.material.color.b,
+				1);
+			} else {
+				renderer.material.color = new Color (
+				renderer.material.color.r,
+				renderer.material.color.g,
+				renderer.material.color.b,
+				rigidBody.velocity.magnitude / magnitudeForOpaque);
+			}
+		}
+
+	}
+}
