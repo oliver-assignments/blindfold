67be35512ff86427f48210413d7dd24e6cc39519
Oliver <oliverbarnum32@gmail.com>
2016-05-14 19:29:35 -0400

    Add bloodstains disappearing

---
+++ Assets/Blood.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blood : MonoBehaviour {
+
+	public float duration =1;
+	private float timer = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GetComponent<PhotonView> ().isMine) {
+			timer += Time.deltaTime;
+			if (timer > duration) {
+
+				PhotonNetwork.Destroy (gameObject);		
+			}
+		}
+	}
+}
